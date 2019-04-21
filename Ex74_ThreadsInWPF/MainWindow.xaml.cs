d8c81484e4c0735d514501cddec3319209db934c
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2019-04-21 20:26:43 +0200

    items tilføjet

--- Ex74_ThreadsInWPF/MainWindow.xaml.cs
+++ Ex74_ThreadsInWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,56 @@ namespace Ex74_ThreadsInWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnPutIn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbFruits.SelectedItem != null)
+            {
+                var fruit = (lbFruits.SelectedItem as ListBoxItem).Content;
+                lbBlender1.Items.Add(new ListBoxItem { Content = fruit });
+            }
+        }
+
+        private void BtnPutIn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbFruits.SelectedItem != null)
+            {
+                var fruit = (lbFruits.SelectedItem as ListBoxItem).Content;
+                lbBlender2.Items.Add(new ListBoxItem { Content = fruit });
+            }
+        }
+
+        private void BtnBlend1_Click(object sender, RoutedEventArgs e)
+        {
+            Blend1();
+        }
+
+        private void BtnBlend2_Click(object sender, RoutedEventArgs e)
+        {
+            Blend2();
+        }
+
+        private void Blend1()
+        {
+            int blendTime = 10;
+            for (int i = 0; i < blendTime; i++)
+            {
+                lblStatus1.Content = $"Blending {i}";
+                Thread.Sleep(1000);
+            }
+            lblStatus1.Content = "Juice Ready";
+        }
+
+        private void Blend2()
+        {
+            int blendTime = 10;
+            for (int i = 0; i < blendTime; i++)
+            {
+                lblStatus2.Content = $"Blending {i}";
+                Thread.Sleep(1000);
+            }
+            lblStatus2.Content = "Juice Ready";
         }
     }
 }
